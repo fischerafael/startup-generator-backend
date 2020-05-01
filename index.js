c83214d79ed1b6fec5e306643c71fe8c4c64d402
Fischerzera <designfischer@users.noreply.github.com>
2020-05-01 12:03:05 -0300

    configurações para o heroku

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ mongoose.connect(process.env.DB_CONNECT,
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+app.listen(process.env.PORT || 3333, () => console.log('Server is running on port 3333'));
 
